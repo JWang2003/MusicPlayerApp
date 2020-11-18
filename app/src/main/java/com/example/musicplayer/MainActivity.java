88d483b13f91e6e8679201e0d4270a082d4b545f
JWang0105 <jimmywangatschool@gmail.com>
2020-11-18 11:06:12 -0800

    Added songs

--- app/src/main/java/com/example/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/musicplayer/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.musicplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import java.util.ArrayList;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,18 +14,51 @@ public class MainActivity extends AppCompatActivity {
         populateDataModel();
     }
 
+
+
     void populateDataModel() {
+        // Initialize properties of playlist
+        playlist.name = "My Playlist";
+        playlist.songs = new ArrayList<>();
+        // ^ Used to be <Songs> instead of <>
+
+
+        // Create & initialize first song
         Song song = new Song("Acoustic Breeze", "bensound.com",
-                           R.drawable.acousticbreeze, R.raw.acousticbreeze);
+                R.drawable.acousticbreeze, R.raw.acousticbreeze);
+        // Add first song to to array of songs in the playlist
+        playlist.songs.add(song);
+
+
+        // Reusing song variable
+        song = new Song("A New Beginning", "bensound.com",
+                R.drawable.anewbeginning, R.raw.anewbeginning);
+        playlist.songs.add(song);
+
+
+        song = new Song("Creative Minds", "bensound.com",
+                R.drawable.creativeminds, R.raw.creativeminds);
+        playlist.songs.add(song);
+
+
+        song = new Song ("Going Higher", "bensound.com",
+                R.drawable.goinghigher, R.raw.goinghigher);
+        playlist.songs.add(song);
+
+
+        song = new Song ("Happy Rock", "bensound.com",
+                R.drawable.happyrock, R.raw.happyrock);
+        playlist.songs.add(song);
+
+        song = new Song("Hey", "bensound.com",
+                R.drawable.hey, R.raw.hey);
+        playlist.songs.add(song);
+
+        song = new Song("Summer", "bensound.com",
+                R.drawable.summer, R.raw.summer);
+        playlist.songs.add(song);
     }
 
-
-//Properties
+    // Properties
     Playlist playlist = new Playlist();
-
-
-
-
-
-
 }
