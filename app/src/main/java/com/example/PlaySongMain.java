7ab0369c81542c8f67a39afeb487824ea5264b3e
JWang0105 <jimmywangatschool@gmail.com>
2020-11-28 11:24:49 -0800

    music player now fully working. Still need to test loop but everything else is all good. Hana please find image file for both pressedLoopButton and notPressedLoopButton. Thanks.

--- app/src/main/java/com/example/PlaySongMain.java
+++ app/src/main/java/com/example/PlaySongMain.java
@@ -20,12 +20,14 @@ import java.util.Random;
 
 public class PlaySongMain extends AppCompatActivity {
 
+    //Initialize variables
     int mp3Resource;
     Song currentSong;
-
-
     ArrayList<Song> songs;
     int currentSongIndex;
+    //checks if Mp3 was playing before stop
+    boolean isPlaying = false;
+
 
 
     //Initialize mediaPlayer
@@ -33,20 +35,13 @@ public class PlaySongMain extends AppCompatActivity {
 
 
 
-
-    //TODO: [J] Ask william how the heck this stupid displayCurrentSong works
+    //Displays currentSong when switching songs
     private void displayCurrentSong() {
-        /*String songName = currentSong.songName;
-        String artistName = currentSong.artistName;
-        int imageResource = currentSong.imageResource;
-        int mp3Resource = currentSong.mp3Resource;*/
-
+        currentSong = songs.get(currentSongIndex);
         TextView songTitle = findViewById(R.id.songName);
         songTitle.setText(currentSong.songName);
-
         TextView artistTitle = findViewById(R.id.songWriter);
         artistTitle.setText(currentSong.artistName);
-
         ImageView image = findViewById(R.id.songImage);
         image.setImageResource(currentSong.imageResource);
     }
@@ -65,13 +60,13 @@ public class PlaySongMain extends AppCompatActivity {
 
 
         // Go back to mainactivity
-        Button back = findViewById(R.id.back);
+        /*Button back = findViewById(R.id.back);
         back.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent intent = new Intent(PlaySongMain.this, MainActivity.class);
                 startActivity(intent);
             }
-        });
+        });*/
     }
 
     private void getIncomingIntent() {
@@ -100,41 +95,28 @@ public class PlaySongMain extends AppCompatActivity {
 
 
 
+
+
+
+
+
     void setupButtonHandlers() {
-        ImageButton shuffleButton = findViewById(R.id.shuffle);
-
-        shuffleButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Called when shuffle button is tapped
-                System.out.println("shuffle has been tapped");
-                void shuffleSong();
+        //Top bar [Start]
+        //Back Button
+        Button back = findViewById(R.id.back);
+        back.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(PlaySongMain.this, MainActivity.class);
+                startActivity(intent);
+                isPlaying = false;
             }
         });
+        //Top Bar [end]
 
 
-        ImageButton previousButton = findViewById(R.id.prev);
-        previousButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Called when prev button is tapped
-                System.out.println("previous button has been tapped");
-                currentSongIndex--;
-                if (!(mediaPlayer == null)) {
-                    songOver();
-                }
 
-                switchSong(currentSongIndex, currentSongIndex - 1);
-
-                /*if(currentSongIndex >= 0 ) {
-                    currentSongIndex--;
-                } else {
-                    currentSongIndex = (songs.size() - 1);
-                }*/
-            }
-        });
-
-
+        //Song control buttons [Start]
+        //PausePlay Button
         final ImageButton pauseplayButton = findViewById(R.id.pauseplay);
         pauseplayButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -147,70 +129,136 @@ public class PlaySongMain extends AppCompatActivity {
                 } else if (!mediaPlayer.isPlaying()) {
                     playCurrentSong();
                     pauseplayButton.setBackgroundResource(R.drawable.pause);
+                    isPlaying = true;
                     // If mediaPlayer is already playing, pause the song.
                 } else {
                     pauseCurrentSong();
                     pauseplayButton.setBackgroundResource(R.drawable.play);
+                    isPlaying = false;
                 }
             }
 
         });
 
-
-        ImageButton nextButton = findViewById(R.id.next);
-        nextButton.setOnClickListener(new View.OnClickListener() {
+        //PreviousSong Button
+        ImageButton previousSongButton = findViewById(R.id.prev);
+        previousSongButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //Called when next button is tapped
-
+                // Checks if mediaplayer is still playing
                 if (!(mediaPlayer == null)) {
                     songOver();
                 }
-                switchSong(currentSongIndex, currentSongIndex + 1);
-
-                /*if(currentSongIndex < (songs.size() - 1) ) {
-                    currentSongIndex++;
+                if(currentSongIndex > 0 ) {
+                    switchSong(currentSongIndex, currentSongIndex - 1);
+                    /*DEBUG: System.out.println("normal prevSong ");*/
+                    if (isPlaying && mediaPlayer==null) {
+                        playCurrentSong();
+                        pauseplayButton.setBackgroundResource(R.drawable.pause);
+                    }
                 } else {
-                    currentSongIndex = 0;
-                }*/
+                    currentSongIndex = (songs.size() - 1);
+                    System.out.println("index: " + currentSongIndex);
+                    /*DEBUG: System.out.println("special prevSong");*/
+                    displayCurrentSong();
+                    if (isPlaying && mediaPlayer==null) {
+                        playCurrentSong();
+                        pauseplayButton.setBackgroundResource(R.drawable.pause);
+                    }
+                }
             }
         });
 
-        ImageButton loopButton = findViewById(R.id.loop);
+
+
+        //NextSong Button
+        ImageButton nextSongButton = findViewById(R.id.next);
+        nextSongButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            //Called when next button is tapped
+            public void onClick(View view) {
+                //Checks if mediaplayer is already playing. If yes, stops current song.
+                if (!(mediaPlayer == null)) {
+                    songOver();
+                }
+                //Normal song switching, moves up arraylist index by 1.
+                if(currentSongIndex < (songs.size() - 1) ) {
+                    switchSong(currentSongIndex, currentSongIndex + 1);
+                    if (isPlaying && mediaPlayer==null) {
+                        playCurrentSong();
+                        pauseplayButton.setBackgroundResource(R.drawable.pause);
+                    }
+
+                  //set arraylist index to 0 when detecting that currentSongIndex is the max arraylist index already.
+                } else {
+                    currentSongIndex = 0;
+                    System.out.println("index: " + currentSongIndex);
+                    displayCurrentSong();
+                      if (isPlaying && mediaPlayer==null) {
+                        playCurrentSong();
+                        pauseplayButton.setBackgroundResource(R.drawable.pause);
+                    }
+                }
+            }
+        });
+
+        //Shuffle Button
+        ImageButton shuffleButton = findViewById(R.id.shuffle);
+        shuffleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Called when shuffle button is tapped
+                System.out.println("shuffle has been tapped");
+                shuffleSong();
+                if (!(mediaPlayer==null)) {
+                    songOver();
+                }
+                if (isPlaying && mediaPlayer==null) {
+                    playCurrentSong();
+                    pauseplayButton.setBackgroundResource(R.drawable.pause);
+                }
+
+            }
+        });
+
+        //Loop Button
+        final ImageButton loopButton = findViewById(R.id.loop);
+
         loopButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //Called when loop button is tapped
                 System.out.println("loop");
-                if (!(mediaPlayer == null)) {
-                    if (!(mediaPlayer.isLooping())) {
-                        mediaPlayer.setLooping(true);
-                    } else {
-                        mediaPlayer.setLooping(false);
-                    }
-                if (mediaPlayer == null) {
-                    System.out.println("can't loop when music player == null");
-                    }
-                }
+                loopSong();
             }
         });
-
+        //Song control buttons [end]
         }
 
 
 
+    //Function that shuffles songs
     void shuffleSong() {
-        boolean shuffled = true;
-        int upperLimit = songs.size() - 1;
-        Random random = new Random();
-        int rand = random.nextInt(upperLimit);
-        rand = currentSongIndex;
+        //Previous song index tells whether currentSongIndex after random is still the same as before
+        int previousSongIndex;
+        previousSongIndex = currentSongIndex;
+        //Loop prevents shuffling to same song with the help of previousSongIndex
+        while (true) {
+            int upperLimit = songs.size() - 1;
+            Random random = new Random();
+            int rand = random.nextInt(upperLimit);
+            currentSongIndex = rand;
+            System.out.println("Shuffled song. index now at " + currentSongIndex);
+            //Breaks loop once detect that currentSongIndex does not equal previousSongIndex
+            if (currentSongIndex != previousSongIndex) break;
+            }
+        //Displays current song
+        displayCurrentSong();
+        }
 
-        //TODO: [J] apply set display current song for shuffle song
-        setDisplay();
 
-    }
 
+    //Function that plays song
     void playCurrentSong() {
         System.out.println("play @ " + currentSongIndex);
         // Check if media player already exists
@@ -220,33 +268,48 @@ public class PlaySongMain extends AppCompatActivity {
             // Create mediaplayer for current mp3
             mediaPlayer = MediaPlayer.create(PlaySongMain.this, currentSong.mp3Resource);
         }
-        // Play the song
+        // Plays media player mp3
         mediaPlayer.start();
-        //
+        isPlaying = true;
     }
 
 
+    //Function that pauses song
     void pauseCurrentSong() {
         System.out.println("pause @ " + currentSongIndex);
-        // Check if media player already exists
+        // Check if media player already exists, if media player does not exist, it doesn't run.
         if (mediaPlayer != null) {
-
             // Pauses media player mp3
             mediaPlayer.pause();
         }
 
     }
 
+    //Function that is normally called when pressing nextSong/previousSong button.
     void switchSong(int fromIndex, int toIndex) {
         currentSongIndex = toIndex;
-        //TODO: [J] apply display current song to switch song
+        System.out.println("index: " + currentSongIndex);
         displayCurrentSong();
 
     }
 
+    //Function that loops current song
+    void loopSong() {
+        if (!(mediaPlayer == null)) {
+            if (!(mediaPlayer.isLooping())) {
+                mediaPlayer.setLooping(true);
+            } else {
+                mediaPlayer.setLooping(false);
+            }
+            if (mediaPlayer == null) {
+                System.out.println("can't loop when music player == null");
+            }
+        }
+    }
 
 
 
+    // Function that stops current song and completely destroys current media player
     void songOver() {
         mediaPlayer.stop();
         mediaPlayer.reset();
